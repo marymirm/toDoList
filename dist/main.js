1a9e2cf61ad0f3742ac8485c698d95fc511acaf8
Ymir Meijles <ymirmar@live.com>
2022-12-19 18:36:58 +0100

    add collapsible button

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,5 @@
+myToDos = [];
+
 console.log("javascript connected correctly");
 
 class MyListItemClass {
@@ -11,12 +13,12 @@ class MyListItemClass {
 
     setFinished(finished) {
         this.finished = finished;
-    }
+    };
 
     isExpired() {
         let now = new Date ();
         return now > this.dueDate;
-    }
+    };
 
     createTableRow() {
         const table = document.getElementById("table");
@@ -37,25 +39,75 @@ class MyListItemClass {
         const tdFinished = document.createElement("td");
         tdFinished.textContent = this.finished;
 
+        //Collapsible with button
+        const tdCollapsible = document.createElement("td");
+        tdCollapsible.classList.add("tdCollapsible");
+        const collapsible = document.createElement("button")
+        collapsible.classList.add("collapsible");
+        collapsible.innerHTML = "+";
+
+
         tr.appendChild(tdTitle);
         tr.appendChild(tdDescription);
         tr.appendChild(tdDueDate);
         tr.appendChild(tdPriority);
-        tr. appendChild(tdFinished);
+        tr.appendChild(tdFinished);
+        tr.appendChild(tdCollapsible);
+        tdCollapsible.appendChild(collapsible);
+        // tr.appendChild(content)
+        // content.appendChild(pContent);
         table.appendChild(tr);
     }
 }
 
+function clearForm() {
+    title.value = "";
+    description.value = "";
+    dueDate.value = "";
+    priority.value = "";
+};
+
+
 function addToDo() {
-    let title = prompt("Add To Do");
-    const ToDo1 = new MyListItemClass (title, 'desk', new Date(), 1, false);
-    ToDo1.createTableRow();
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let dueDate = document.getElementById("dueDate").value;
+    let priority = document.getElementById("priority").value;
+    
+    const ToDo = new MyListItemClass (title, description, dueDate, priority);
+    myToDos.push(ToDo);
+    console.log("To do item added to", ToDo, "and pushed to", myToDos)
+    ToDo.createTableRow();
+    clearForm();
+};
+
+
+// Functionality for Collapsible
+let coll = document.getElementsByClassName("table");
+let i;
+
+console.log("Collapsible wired")
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+    content.style.display = "none";
+    } else {
+    content.style.display = "block";
+    }
+    
+});
 }
+
+
+
+
 
 // test form
 function toggleForm(){
     document.body.classList.toggle('activeForm');
-}
+};
 
 
 //Function that renders different To Do Lists
